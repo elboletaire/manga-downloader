516c035751a98781a4202cbfaa10c7108c65753a
Òscar Casajuana <elboletaire@gmail.com>
2023-01-18 19:38:30 +0100

    Added support for manganelos.com
    Also fixed support for manganelo.tv, which was broken at some point
    
    closes #9

--- grabber/manganelo.go
+++ grabber/manganelo.go
@@ -36,13 +36,25 @@ func (m *Manganelo) Test() (bool, error) {
 		return false, err
 	}
 
-	// manganelo style
-	m.rows = m.doc.Find("div.panel-story-chapter-list .row-content-chapter li")
-	if m.rows.Length() > 0 {
-		return true, nil
+	// order is important, since some sites have very similar selectors
+	selectors := []string{
+		// manganelo/manganato style
+		"div.panel-story-chapter-list .row-content-chapter li",
+		// manganelos style (not using the parent id returns more stuff)
+		"#examples div.chapter-list .row",
+		// mangakakalot style
+		"div.chapter-list .row",
 	}
-	// mangakakalot style
-	m.rows = m.doc.Find("div.chapter-list div.row")
+
+	// for the same priority reasons, we need to iterate over the selectors
+	// using a simple `,` joining all selectors would resturn missmatches
+	for _, selector := range selectors {
+		rows := m.doc.Find(selector)
+		if rows.Length() > 0 {
+			m.rows = rows
+			break
+		}
+	}
 
 	return m.rows.Length() > 0, nil
 }
@@ -95,32 +107,47 @@ func (m Manganelo) FetchChapter(f Filterable) (*Chapter, error) {
 		return nil, err
 	}
 
-	pimages := doc.Find("div.container-chapter-reader img")
+	pimages := getImageUrls(doc)
+
 	chapter := &Chapter{
 		Title:      f.GetTitle(),
 		Number:     f.GetNumber(),
-		PagesCount: int64(pimages.Length()),
+		PagesCount: int64(len(pimages)),
 		Language:   "en",
 	}
 
-	// get the chapter pages
-	doc.Find("div.container-chapter-reader img").Each(func(i int, s *goquery.Selection) {
-		u := s.AttrOr("src", "")
-		n := int64(i)
-		if u == "" {
+	for i, img := range pimages {
+		if img == "" {
 			// this error is not critical and is not from our side, so just log it out
-			color.Yellow("page %d of %s has no URL to fetch from 😕 (will be ignored)", n, chapter.GetTitle())
-			return
+			color.Yellow("page %d of %s has no URL to fetch from 😕 (will be ignored)", i, chapter.GetTitle())
+			continue
 		}
-		if !strings.HasPrefix(u, "http") {
-			u = m.BaseUrl() + u
+		if !strings.HasPrefix(img, "http") {
+			img = m.BaseUrl() + img
 		}
 		page := Page{
-			Number: n,
-			URL:    u,
+			Number: int64(i),
+			URL:    img,
 		}
 		chapter.Pages = append(chapter.Pages, page)
-	})
+	}
 
 	return chapter, nil
+}
+
+func getImageUrls(doc *goquery.Document) []string {
+	// some sites store a plain text array with the urls into a hidden layer
+	pimages := doc.Find("#arraydata")
+	if pimages.Length() == 1 {
+		return strings.Split(pimages.Text(), ",")
+	}
+
+	// others just have the images
+	pimages = doc.Find("div.container-chapter-reader img")
+	imgs := []string{}
+	pimages.Each(func(i int, s *goquery.Selection) {
+		imgs = append(imgs, s.AttrOr("src", s.AttrOr("data-src", "")))
+	})
+
+	return imgs
 }
