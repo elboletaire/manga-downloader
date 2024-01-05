ff7f9f55f0fc355d1186d9b608966dbda49e08ef
Òscar Casajuana <elboletaire@gmail.com>
2024-01-05 23:39:52 +0100

    Add Dockerfile, gh action docker workflow and output-dir param
    refs #23

--- packer/pack.go
+++ packer/pack.go
@@ -2,6 +2,7 @@ package packer
 
 import (
 	"fmt"
+	"path/filepath"
 
 	"github.com/elboletaire/manga-downloader/downloader"
 	"github.com/elboletaire/manga-downloader/grabber"
@@ -14,27 +15,27 @@ type DownloadedChapter struct {
 }
 
 // PackSingle packs a single downloaded chapter
-func PackSingle(s grabber.Site, chapter *DownloadedChapter) (string, error) {
+func PackSingle(outputdir string, s grabber.Site, chapter *DownloadedChapter) (string, error) {
 	title, _ := s.FetchTitle()
-	return pack(s.GetFilenameTemplate(), title, NewChapterFileTemplateParts(title, chapter.Chapter), chapter.Files)
+	return pack(outputdir, s.GetFilenameTemplate(), title, NewChapterFileTemplateParts(title, chapter.Chapter), chapter.Files)
 }
 
 // PackBundle packs a bundle of downloaded chapters
-func PackBundle(s grabber.Site, chapters []*DownloadedChapter, rng string) (string, error) {
+func PackBundle(outputdir string, s grabber.Site, chapters []*DownloadedChapter, rng string) (string, error) {
 	title, _ := s.FetchTitle()
 	files := []*downloader.File{}
 	for _, chapter := range chapters {
 		files = append(files, chapter.Files...)
 	}
 
-	return pack(s.GetFilenameTemplate(), title, FilenameTemplateParts{
+	return pack(outputdir, s.GetFilenameTemplate(), title, FilenameTemplateParts{
 		Series: title,
 		Number: rng,
 		Title:  "bundle",
 	}, files)
 }
 
-func pack(template, title string, parts FilenameTemplateParts, files []*downloader.File) (string, error) {
+func pack(outputdir, template, title string, parts FilenameTemplateParts, files []*downloader.File) (string, error) {
 	filename, err := NewFilenameFromTemplate(template, parts)
 	if err != nil {
 		return "", fmt.Errorf("- error creating filename for chapter %s: %s", title, err.Error())
@@ -42,7 +43,7 @@ func pack(template, title string, parts FilenameTemplateParts, files []*download
 
 	filename += ".cbz"
 
-	if err = ArchiveCBZ(filename, files); err != nil {
+	if err = ArchiveCBZ(filepath.Join(outputdir, filename), files); err != nil {
 		return "", fmt.Errorf("- error saving file %s: %s", filename, err.Error())
 	}
 
