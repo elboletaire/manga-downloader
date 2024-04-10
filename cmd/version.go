6ae714eb5459018382d20525bd2a0189855b2620
Òscar Casajuana <elboletaire@gmail.com>
2024-04-10 20:08:38 +0200

    Add version checker (via version command)

--- cmd/version.go
+++ cmd/version.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/fatih/color"
 	"github.com/spf13/cobra"
+	"github.com/tcnksm/go-latest"
 )
 
 var (
@@ -21,7 +22,29 @@ var versionCmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Printf("%s - Manga volumes downloading tool\n", color.YellowString("Manga Downloader"))
 		fmt.Printf("All Rights Reserved © 2023-2024 %s\n", color.HiBlackString("Òscar Casajuana Alonso"))
-		fmt.Printf("Version: %s\n", color.MagentaString("%s (%s)", Version, Tag))
+		fmt.Printf("Version: %s - ", color.MagentaString("%s (%s)", Version, Tag))
+
+		vcheck := &latest.GithubTag{
+			Owner:             "elboletaire",
+			Repository:        "manga-downloader",
+			FixVersionStrFunc: latest.DeleteFrontV(),
+		}
+
+		res, err := latest.Check(vcheck, Tag)
+		if err != nil {
+			fmt.Printf("Error checking for updates: %s\n", err)
+			return
+		}
+		if res.Outdated {
+			fmt.Printf(
+				"%s Download latest (%s) from:\n%s\n",
+				color.HiRedString("App is outdated."),
+				color.RedString(res.Current),
+				"https://github.com/elboletaire/manga-downloader/releases/tag/v"+res.Current,
+			)
+		} else {
+			fmt.Printf("%s\n", color.GreenString("App is up to date."))
+		}
 	},
 }
 
